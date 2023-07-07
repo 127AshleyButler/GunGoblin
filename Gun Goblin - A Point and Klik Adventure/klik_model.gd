d597d711ab0248124d3696eb02c30e98f1c18930
DaveDood2 <artistic109@gmail.com>
2023-07-07 15:46:09 -0500

    Fixed klik's rotation
    klik lost her blender heritage

--- Gun Goblin - A Point and Klik Adventure/klik_model.gd
+++ Gun Goblin - A Point and Klik Adventure/klik_model.gd
@@ -6,6 +6,15 @@ extends Node3D
 func _ready():
 	pass # Replace with function body.
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if (input_dir):
+		anim.play("run")
+		var direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		look_at(global_transform.origin + direction, Vector3.UP, true)
+	else: anim.play("idle")
+	
 func _getWASD():
 	if Input.is_action_pressed("move_down"):
 		return true;
@@ -16,14 +25,3 @@ func _getWASD():
 	if Input.is_action_pressed("move_up"):
 		return true;
 	return false;
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if (input_dir):
-		anim.play("run")
-		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		look_at(global_transform.origin + direction, Vector3.UP, true)
-
-	else: anim.play("idle")
-	
