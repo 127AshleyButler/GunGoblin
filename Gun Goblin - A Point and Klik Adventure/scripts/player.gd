7f177ea959ac5aea12bd7696063cf3e26c457f0c
DaveDood2 <davejbutler99@gmail.com>
2023-11-16 18:35:23 -0600

    added sit animation

--- Gun Goblin - A Point and Klik Adventure/scripts/player.gd
+++ Gun Goblin - A Point and Klik Adventure/scripts/player.gd
@@ -26,6 +26,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
 		handle_jump()
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -51,7 +52,10 @@ func update_animation_parameters():
 	$AnimationTree["parameters/run/blend_position"] = facing
 	$AnimationTree["parameters/rise/blend_position"] = facing
 	$AnimationTree["parameters/fall/blend_position"] = facing
+	$AnimationTree["parameters/sit/blend_position"] = facing
 
 	$AnimationTree["parameters/conditions/idle"] = (velocity == Vector3.ZERO)
+	$AnimationTree["parameters/conditions/holding_down"] = Input.is_action_pressed("move_down")
+	$AnimationTree["parameters/conditions/not_holding_down"] = !Input.is_action_pressed("move_down")
 
 	
