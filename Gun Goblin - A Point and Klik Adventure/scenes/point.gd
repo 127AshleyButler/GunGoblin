ba80b858e5b8d37aab656dd3e7056ad957b6ad54
Ashley Butler <127ashleybutler@gmail.com>
2023-12-01 16:44:53 -0600

    Point now has animation player thingie

---
+++ Gun Goblin - A Point and Klik Adventure/scenes/point.gd
@@ -0,0 +1,11 @@
+extends Control
+@onready var _point = $Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_point.position = get_viewport().get_mouse_position()
